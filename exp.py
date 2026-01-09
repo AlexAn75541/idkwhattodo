6074b70233e6f9809a0db92d038d3aaaf44f8822
Aretzera <agroup168@gmail.com>
2026-01-09 10:22:56 +0700

    another1

--- exp.py
+++ exp.py
@@ -1,4 +1,3 @@
-hour = input("Enter hour: ")
-rate = input("Enter rate: ")
-
-print("Pay:", int(hour) * float(rate))
+name = input("Enter your name: ")
+year= int(input("Enter your year of choice: "))
+print(f"{name} is a valiant knight, born in the year {year}. One morning, Mary woke up to an awful racket: a dragon was approaching the village. Only {name} could save the village's residents.")
