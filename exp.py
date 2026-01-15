828cd6b8054443c47b5ddbc1126b63c7f0cc137a
Aretzera <agroup168@gmail.com>
2026-01-15 16:45:56 +0700

    Try-ELIF

--- exp.py
+++ exp.py
@@ -7,4 +7,10 @@ elif x == 2:
 else :
     print("x is not greater than 2")
     
+try :
+    y = 10 / 0
+except ZeroDivisionError :
+    print("You can't divide by zero!")    
+    
+
 print("This line is always executed")
