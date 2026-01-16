57889c70c8581b23da46b1a74b1b8fa92533f3f9
Aretzera <agroup168@gmail.com>
2026-01-16 08:04:28 +0700

    15/01/2025

--- exp.py
+++ exp.py
@@ -1,16 +1,24 @@
-x = 23
+def numeric_input_checker(prompt):
+    while True:
+        try:
+            value = float(input(prompt))
+            return value
+        except ValueError:
+            print("Invalid input. Please enter a numeric value.")
+hours = numeric_input_checker("Enter hours: ")
+rate = numeric_input_checker("Enter rate: ")
 
-if x > 2 :
-    print("x is greater than 2")
-elif x == 2:
-    print("x is equal to 2")
+if hours > 40:
+    overtime = hours - 40
+    pay = (40 * rate) + (overtime * rate * 1.5)
+    print(f"Pay: {pay:.2f}")
 else :
-    print("x is not greater than 2")
-    
-try :
-    y = 10 / 0
-except ZeroDivisionError :
-    print("You can't divide by zero!")    
-    
-
-print("This line is always executed")
+    try :
+        reconfirm = input(f"Are you sure you worked only {hours} hours? ")
+        if reconfirm =="yes" or reconfirm == "y" :
+            print(f"Pay: {hours * rate:.2f}")
+        else :
+            loop = True
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    # print(f"Pay: {hours * rate:.2f}")
