0c1aa5a6fc4a5b42631ce4725d9f3c63cef9c076
Aretzera <agroup168@gmail.com>
2026-01-22 16:38:07 +0700

    the countdown

--- exp.py
+++ exp.py
@@ -1,13 +1,3 @@
-while True:
-    line = input("Enter a line of text (or 'exit' to quit): ")
-    if line.lower() == 'exit':
-        continue
-    if line[0] == '#':  # the first character is a comment
-        continue
-    print(f"You entered: {line}")
-    try:
-        x = float(input("Enter an integer: "))
-        break
-    except ValueError:
-        print("Invalid input. Please enter a valid integer.")
-print(f"You entered the integer: {x}")
+for nigger in range(5, 0, -1) : # start, end, steps
+    print (nigger)
+print('nigger1')
