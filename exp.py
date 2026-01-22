0ed5f0ef8fdca65a424af3a55a0dc9cb6b378959
Aretzera <agroup168@gmail.com>
2026-01-22 17:42:31 +0700

    another one but minus + inf

--- exp.py
+++ exp.py
@@ -1,9 +1,14 @@
+
+
+
+from math import inf
+
+
 print('Before')
-Largest = -1
-for i in [9, 41, 12, 3, 74, 15]:
+Largest = -inf
+for i in [-9, -41, -12, -3, -74, -15]:
     if i > Largest:
         Largest = i
-        print ('New Largest:', Largest)
     print('nice', i)
 
 print('Largest:', Largest)
