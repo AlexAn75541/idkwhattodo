069db3dfdad1fe04a0b2f8e5e3c71d3b4e9902cf
Aretzera <agroup168@gmail.com>
2026-01-29 16:39:45 +0700

    idk about this one lol, shits so fucking hard

--- exp.py
+++ exp.py
@@ -1,6 +1,13 @@
-word = 'banana'
-count = 0
-for letter in word:
-    if letter == 'a':
-        count = count + 1
-print(count)
+word = input("Fucking this banana so hard: ")
+
+
+if word == 'banana':
+    print('This is a banana.')
+    
+if word < 'banana':
+    print("The word " + word + " comes before banana.")
+elif word > 'banana':
+    print("The word " + word + " comes after banana.")
+else:
+    print('The word is banana.')
+    
