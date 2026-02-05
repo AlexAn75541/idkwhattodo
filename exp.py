a88ac31a59c15dd13995d946599284916c3d3953
Aretzera <agroup168@gmail.com>
2026-02-05 16:57:08 +0700

    Some more of that ranged list

--- exp.py
+++ exp.py
@@ -1,3 +1,11 @@
-lotto = [1, 2, 3, 4]
-lotto[2] = 28
-print(lotto)
+# range (5, 0, -1)
+# its start - end -step
+
+
+friend = ['nick', 'sam', 'jake', 'tom']
+#print(list((friend[1:])))
+    
+# friends = list()
+# friends.append(friend)
+friend.sort(reverse=True)
+print(friend)
