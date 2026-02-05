dc7ddf452b5c63e2ff79b1568c2776dbc98a06bc
Aretzera <agroup168@gmail.com>
2026-02-05 10:47:53 +0700

    Bit practice with the list [PRE MIDTERM TEST]

---
+++ exp.py
@@ -0,0 +1,15 @@
+
+list = []
+inputs = str(input("Enter something: "))
+while inputs != "":
+    list.append(inputs)
+    inputs2 = str(input("Enter something: "))
+    inputs3 = str(input("Enter something: "))
+    inputs4 = str(input("Enter something: "))
+    list.append(inputs2)
+    list.append(inputs3)
+    list.append(inputs4)
+    break
+
+list.
+print(list)
