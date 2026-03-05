f5b5671f5dfa5d1c78a4cb201e58f28ba5881aeb
Aretzera <agroup168@gmail.com>
2026-03-05 15:52:14 +0700

    dict 1st

--- hell.py
+++ hell.py
@@ -1,14 +1,20 @@
-import re
+# pithon dictionary
+cabinet = dict ()
+cabinet['summer'] = 12
+cabinet['winter'] = 24
+cabinet['spring'] = 6
+cabinet['fall'] = 'yes'
+cabinet['summer'] = cabinet['summer'] + 1 # update the value of summer to 18
+print(cabinet)
+print(cabinet['summer'])
+print(cabinet['winter'])
+print(cabinet['spring'])
+print(cabinet['fall'])
 
-nigga = open('mbox-short.txt')
-numlist = list()
-for line in nigga:
-    line = line.rstrip()
-    #if re.search('From:', line):
-       # print(line)
-    #if re.findall('From:(\\S+@[^ ]*\\S)', line):
-        #print(line)
-    stuff = re.findall('X-DSPAM-Confidence: $([0-9.]+)', line)
-    if len(stuff) > 0:
-       numlist.append(float(stuff[0]))
-print('Maximum:', (numlist)) # min
+
+# list:
+#cards = list()
+#cards.append("Ace of Spades")
+#cards.append("2 of Hearts")
+#cards.insert(1, "3 of Diamonds") 
+#print(cards)
