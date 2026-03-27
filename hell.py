cf421c53d2a9c774c2fa9e805613210bb39154db
Aretzera <agroup168@gmail.com>
2026-03-27 16:49:28 +0700

    9th

--- hell.py
+++ hell.py
@@ -1,41 +1,12 @@
 
-
-
-
-class creditcard:
-    @creditcard1
-    def __init__(self, number="", CVV=0, expiry_date=""):
-        self.number = number
-        self.CVV = CVV
-        self.expiry_date = expiry_date
-
-
-
-class User:
-    @User1Nig
-    def __init__(self, name):
-        self.name = "An"
-        self.credit_card = []
-        
-    def add_card(self, card):
-        self.credit_card.append(card)
-        
-    def print_name(self):
-        print(f"User Name: {self.name}")
-        
-    def print_last_four_digits(self):
-        for card in self.credit_card:
-            print(f"Last four digits of card: {card.number[-4:]}")
-        
-card1 = creditcard(number=input("Enter credit 1st card number: "))
-card2 = creditcard(number=input("Enter credit 2nd card number: "))
-card3 = creditcard(number=input("Enter credit 3rd card number: "))
-
-
-user = User()
-user.print_name()
-user.add_card(card1)
-user.add_card(card2)
-user.add_card(card3)
-
-user.print_last_four_digits()
+try:
+    nword = open("mbox-short.txt")
+except:
+    print("Error occurred while reading the file.")
+    quit()
+    
+count = 0
+for line in nword:
+    if line.startswith('Subject'):
+        count += 1
+print(f"Number of lines starting with 'Subject': {count}")
