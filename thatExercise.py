428cfb2fba92c2582fb161f653a70921b17cabd0
Aretzera <agroup168@gmail.com>
2026-04-09 22:04:42 +0700

    I hate my life

--- thatExercise.py
+++ thatExercise.py
@@ -1,28 +1,16 @@
-def numeric_input_checker(prompt):
-    while True:
-        try:
-            value = float(input(prompt))
-            return value
-        except ValueError:
-            print("Invalid input. Please enter a numeric value.")
-            
-hours = numeric_input_checker("Enter hours: ")
-rate = numeric_input_checker("Enter rate: ")
-bonus = numeric_input_checker("Enter bonus (if any, else enter 0): ")
-taxRate = numeric_input_checker("Enter tax rate(percent): ")
-
-def computepay(hours, rate):
-    if hours > 40:
-        overtime = hours - 40
-        pay = (((40 * rate) + (overtime * rate * 1.5)) + bonus) * (1 - taxRate / 100)
-    elif hours <= 40:
-         reconfirm = input(f'Are you sure you worked only {hours} hours? (yes/no): ')
-         if reconfirm.lower() in ('yes', 'y'):
-            pay = hours * rate * (1 - taxRate / 100)
-    else:
-        pay = hours * rate * (1 - taxRate / 100)
-    return pay
+class checker:
+    def __init__(self, fileName):
+       self.fileName = fileName
 
 
-pay = computepay(hours, rate)
-print(f"Pay: {pay}")
+    def openFiles(self):
+        with open (self.fileName) as f:
+            for line in f:
+                if line.startswith("From: "):
+                    print(line.strip())
+        
+
+
+
+filechecker = checker("mbox-short.txt")
+filechecker.openFiles()
